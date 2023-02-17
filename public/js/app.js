36c78353fc9c520b7b8e115979002705d869acd4
Christophe Gouteux <chris.gouteux@outlook.fr>
2023-02-17 22:39:35 +0100

    add animation cube and pb slide title

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+bulmaCarousel.attach('#carousel-title', {
+    autoplay: true,
+    // loop: true,
+    // navigation: false,
+    // pagination: false,
+});
+                                                                       
